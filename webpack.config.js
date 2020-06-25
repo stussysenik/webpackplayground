23b9d377738944ae895742957187d99286fba778
Senik <senik@Seniks-MacBook-Pro.local>
2020-06-25 11:41:11 +0200

    Ouput Management

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,66 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+    mode: 'development',
+    entry: {
+        app: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true,
     },
     module: {
         rules: [{
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ],
-            }, {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader',
-                ],
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader',
-                ],
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                    'csv-loader',
-                ],
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader',
-                ],
-            },
-        ],
-    }
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+        }, ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    // module: {
+    //     rules: [{
+    //             test: /\.css$/,
+    //             use: [
+    //                 'style-loader',
+    //                 'css-loader',
+    //             ],
+    //         }, {
+    //             test: /\.(png|svg|jpg|gif)$/,
+    //             use: [
+    //                 'file-loader',
+    //             ],
+    //         },
+    //         {
+    //             test: /\.(woff|woff2|eot|ttf|otf)$/,
+    //             use: [
+    //                 'file-loader',
+    //             ],
+    //         },
+    //         {
+    //             test: /\.(csv|tsv)$/,
+    //             use: [
+    //                 'csv-loader',
+    //             ],
+    //         },
+    //         {
+    //             test: /\.xml$/,
+    //             use: [
+    //                 'xml-loader',
+    //             ],
+    //         },
+    //     ],
+    // }
 };
